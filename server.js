1757e53a47d3aa861c4adfc826dfe1509c9a908a
Victor Balan <victor.balan93@gmail.com>
2016-05-10 15:23:42 +0300

    authorization
     * add passport
     * add user model
     * add user routes

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(req, res) {
-  res.json({message: 'Hello world!'})
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(passport.initialize());
+
+mongoose.connect('mongodb://localhost:27017/event-planner');
+
+var router = express.Router();
+require('./routes')(router);
+app.use('/', router);
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
